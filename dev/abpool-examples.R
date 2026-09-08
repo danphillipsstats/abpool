a2131c9b811f849755b507fc777607fd13f3a0e6
Daniel J Phillips <89460132+danphillipsstats@users.noreply.github.com>
2026-09-08 15:39:37 +0100

    Added SUPPORT example - Cox fit using mice

--- dev/abpool-examples.R
+++ dev/abpool-examples.R
@@ -14,6 +14,8 @@ lapply(fits,vcov)
 lapply(fits,df.residual)
 
 # list - logistic regression
+require(mice)
+require(survival)
 n <- 100
 X <- rnorm(n)
 Y <- rbinom(n,size=1,prob=plogis(1+3*X + rnorm(n)))
@@ -29,6 +31,30 @@ lapply(fits,vcov)
 lapply(fits,df.residual)
 
 # list - Cox regression
+Hmisc::getHdata(support) # getHdata(support, "all") to view data dictionary
+cox.support.form.char <- "Surv(d.time, death) ~ age + num.co + scoma + alb + bili + pafi + crea + meanbp + hrt + resp + temp + adlsc + dzgroup"
+support.data <- support[,all.vars(as.formula(cox.support.form.char))]
+m <- 200
+t1 <- Sys.time()
+support.data$H0_NA <- mice::nelsonaalen(support.data, timevar = "d.time", statusvar = "death")
+pred <- mice::make.predictorMatrix(support.data)
+# choose methods: do not impute death or H_NA; impute others with pmm
+method <- make.method(support.data)
+for (v in names(method)) {
+  method[v] <- "pmm"
+}
+pred[, "d.time"] <- 0 # do not predict using d.time
+# use H0_NA and death to predict other vars
+pred[, "H0_NA"] <- 1
+pred[, "death"] <- 1
+pred["H0_NA", ] <- 0
+pred["death", ] <- 0
+imp_data_support <- mice(support.data, m = m, method = method, predictorMatrix = pred)
+support.cox.imp <- with(imp_data_support,coxph(as.formula(cox.support.form.char)))
+support.cox.imp
+lapply(support.cox.imp$analyses,coef)
+lapply(support.cox.imp$analyses,vcov)
+lapply(support.cox.imp$analyses,df.residual)
 
 # mice
 library(mice)
