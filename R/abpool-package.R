215da8c728cb2c6230ba02f25c8773182a67cf4f
Daniel J Phillips <89460132+danphillipsstats@users.noreply.github.com>
2026-09-21 17:12:31 +0100

    Fixed function import from stats

--- R/abpool-package.R
+++ R/abpool-package.R
@@ -1,4 +1,6 @@
+#' abpool: Approximate Bayesian Pooling for Multiple Imputation
+#'
 #' @keywords internal
+#' @importFrom stats coef df.residual ppoints qqline qqplot qt quantile
+#'   rnorm rt var vcov
 "_PACKAGE"
-
-#' @importFrom stats coef df.residual ppoints qqline qqplot qt quantile rnorm rt var vcov
