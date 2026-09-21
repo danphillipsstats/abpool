62aa16f4947eeb840fd1fd31a45285528fd36d47
Daniel J Phillips <89460132+danphillipsstats@users.noreply.github.com>
2026-09-21 16:44:41 +0100

    Updated documentation

--- R/abpool_sample.R
+++ R/abpool_sample.R
@@ -13,7 +13,7 @@
 #' @return A numeric vector of posterior draws for a scalar parameter, or a
 #' matrix of posterior draws for multiple parameters.
 #'
-#' @references Phillips, Christodoulou and Steinsaltz (XXXX)
+#' @references Phillips, Christodoulou and Steinsaltz (2026)
 #'
 #' @examples
 #' # Scalar input
